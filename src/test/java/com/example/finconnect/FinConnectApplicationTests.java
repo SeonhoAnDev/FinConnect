5e7ff03ea2eaf4e4fc5c907d2eaeccec0f04efc2
sonho_an <seonhoan022@gmail.com>
2025-01-13 16:23:25 +0900

    FinConnectプロジェクト

---
+++ src/test/java/com/example/finconnect/FinConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
