5f8fb7d327351e70f60289fb5ac7b52b0b0f0062
sonho_an <seonhoan022@gmail.com>
2025-01-19 13:43:30 +0900

    プロントエンド連動

--- src/main/resources/docker/runDocker.sh
+++ src/main/resources/docker/runDocker.sh
@@ -1,10 +1,10 @@
-docker run --name finConnet-postgres \
+docker run --name finConnect-postgres \
 -e POSTGRES_INITDB_ARGS="--data-checksums -E UTF8" \
--e POSTGRES_USER=finConnet-master \
+-e POSTGRES_USER=finConnect-master \
 -e POSTGRES_PASSWORD=root \
--e POSTGRES_DB=finConnet-db \
+-e POSTGRES_DB=finConnect-db \
 -e TZ=Asia/Tokyo \
--v $HOME/Desktop/javaProject/postgres/finConnet-data:/var/lib/postgresql/data \
+-v $HOME/Desktop/javaProject/postgres/finConnect-data:/var/lib/postgresql/data \
 -p 5432:5432 \
 -d postgres:latest
 
